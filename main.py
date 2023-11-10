2e9446c5f802d14a82e56da9c065e512346ee3f5
Monika <monika.belkaite@gmail.com>
2023-11-10 20:50:29 +0200

    generated good looking collage

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import json
 from PIL import Image, ImageDraw, ImageFont
 import os
+import numpy
 
 
 class WardrobeItem:
@@ -35,53 +36,64 @@ collage_directory = "collage_folder"
 if not os.path.exists(collage_directory):
     os.makedirs(collage_directory)
 
-def load_images(items):
-    return [Image.open(item.image_path) for item in items]
+def resize_images(items):
+    resized_images = []
+    for item in items:
+        img = Image.open(item.image_path)
+        img = img.resize((1080, 1080))
+        resized_images.append(img)
+    return resized_images
 
-def create_collage_background(collage_size):
-    collage = Image.new('RGB', (collage_size, collage_size + 50), 'white')
-    return collage, ImageDraw.Draw(collage)
+def create_background():
+    background = Image.new('RGB', (3080, 3080), 'white')
+    return background
 
-def draw_title(draw, title):
-    font = ImageFont.load_default()  # Use a default font
-    draw.text((10, 10), title, fill="black", font=font)
+def draw_title(draw):
+    font = ImageFont.truetype("ARIAL.TTF", 100)
+    title = "Test"
+    text_length = draw.textlength(title, font=font)
+    image_width = 3080
+    text_x = (image_width - text_length) / 2 
+    text_y = 250
+    draw.text((text_x, text_y), title, fill="black", font=font)
 
-def place_images_on_collage(draw, images, collage_size, collage):
-    single_image_size = collage_size // len(images)
-    for i, image in enumerate(images):
-        image.thumbnail((single_image_size, single_image_size))
-        x = i % 2 * single_image_size
-        y = 50  # Offset by the height of the title
-        collage.paste(image, (x, y))
-        border_color = 'black'
-        border_width = 5
-        draw.rectangle([x, y, x + single_image_size, y + single_image_size], outline=border_color, width=border_width)
+def place_images(items):
+    images = resize_images(items)
+    # Convert all images to the same mode (e.g., RGB) to ensure same number of channels
+    images = [img.convert('RGB') for img in images[:4]]
+    image_arrays = [numpy.array(img) for img in images]
+    h1 = numpy.hstack((image_arrays[0], image_arrays[1]))
+    h2 = numpy.hstack((image_arrays[2], image_arrays[3]))
+    array_image = numpy.vstack((h1, h2))
+    final_image = Image.fromarray(array_image)
+    final_image.save(os.path.join(collage_directory, 'test.jpg'))
+    return final_image
 
-def create_collage(items, title, occasion, collage_size=600):
-    images = load_images(items)
-    collage, draw = create_collage_background(collage_size)
-    draw_title(draw, title)
-    place_images_on_collage(draw, images, collage_size, collage)
-
-    collage_filename = f"{title.replace(' ', '_')}_{occasion}.jpg"
-    collage_path = os.path.join(collage_directory, collage_filename)
-    collage.save(collage_path)
-    collage.show()
-    return collage_path
+def create_collage(background, collage):
+    background_width, background_height = background.size
+    collage_width, collage_height = collage.size
+    x = (background_width - collage_width) // 2
+    y = (background_height - collage_height) // 2
+    background.paste(collage, (x, y))
+    return background
 
 
 if __name__ == "__main__":
     season_choice = input("What's your season? Choose between: winter, spring, summer, autumn: ")
     occasion_choice = input("What's your ocassion? Choose between: everyday, business, festive: ")
     wardrobe_items = load_wardrobe_items('wardrobe.json')
-    items_images = load_images(wardrobe_items)
-    for img in items_images:
-        print(img.size)
+    items_images = resize_images(wardrobe_items)
     season_item = get_items_by_season(wardrobe_items, season_choice)
     ocassion_item = get_items_by_occasion(wardrobe_items, occasion_choice)
-    
-    # Create a collage of all items for the specific occasion
-    collage_path = create_collage(wardrobe_items, "My Everyday Outfit", ocassion_item)
+    background = create_background()
+    draw = ImageDraw.Draw(background)
+    collage_title = draw_title(draw)
+    collage = create_collage(background, place_images(wardrobe_items))
+    collage_filename = "test.jpg"
+    collage_path = os.path.join(collage_directory, collage_filename)
+    create_collage(collage, place_images(wardrobe_items))
+    collage.save(collage_path)
+    collage.show()
     print(f"Collage saved to {collage_path}")
 
 
